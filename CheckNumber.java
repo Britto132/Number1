12a01f640c4ca51d0a0f90c90d073d0bc8348bc2
Britto132 <01fe23bca132@kletech.ac.in>
2025-06-09 10:33:58 +0530

    Update CheckNumber.java

--- CheckNumber.java
+++ CheckNumber.java
@@ -1,11 +1,12 @@
-public class CheckNumber{
+public class CheckNumber {
     public static void main(String[] args) {
-        int number =-7;
-        if(number>0){
+        int number = - 7;
+        if (number > 0) {
             System.out.println("Number is positive");
+        } else if (number < 0) {
+            System.out.println("Number is negative");
+        } else {
+            System.out.println("Number is zero");
         }
-        else{
-            System.out.println("Number is either negative or zero");
-        }
-}
+    }
 }
